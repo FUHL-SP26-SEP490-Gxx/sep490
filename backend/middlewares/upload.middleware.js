9ef1d5fa2800858f5f3c7c8c5358e97ed6fff52e
Chu Thế Văn <chuthevan450@gmail.com>
2026-02-03 22:41:52 +0700

    Delete

--- backend/middlewares/upload.middleware.js
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-import path from "path";
-
-const storage = multer.memoryStorage(); // lưu tạm trong memory
-const upload = multer({ storage });
-
-export default upload;
